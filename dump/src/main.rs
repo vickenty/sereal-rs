aaf02db4d7609c86eb0b7cb2f3968154a8410018
Vickenty Fesunov <kent@setattr.net>
2017-04-18 21:44:31 +0200

    Add a simple dumper program.

---
+++ dump/src/main.rs
@@ -0,0 +1,34 @@
+extern crate clap;
+extern crate sereal_decoder;
+
+use std::io::stdout;
+use std::fs::File;
+use std::io::Result;
+use std::io::Write;
+
+use clap::App;
+use clap::Arg;
+
+use sereal_decoder::arc::ArcBuilder;
+use sereal_decoder::parse;
+
+fn main() {
+    let matches = App::new("sereal-dump")
+        .version("0.1.0")
+        .arg(Arg::with_name("input")
+             .required(true)
+             .index(1))
+        .get_matches();
+
+    let fname = matches.value_of("input").unwrap();
+    if let Err(err) = process(fname) {
+        writeln!(stdout(), "{}: {}", fname, err).unwrap();
+    }
+}
+
+fn process(name: &str) -> Result<()> {
+    let file = File::open(name)?;
+    let value = parse(file, ArcBuilder)?;
+    println!("{:#?}", value);
+    Ok(())
+}
