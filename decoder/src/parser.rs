c3cac3f983177779ba1a4907088c8f1f6beedca6
Vickenty Fesunov <kent@setattr.net>
2017-05-01 22:49:58 +0200

    Merge parser with lexer.
    Speeds up parsing by about 25%.

--- decoder/src/parser.rs
+++ decoder/src/parser.rs
@@ -1,25 +1,29 @@
+use std::io;
 use std::result;
 use std::collections::HashMap;
 
+use byteorder::LittleEndian;
+use byteorder::ByteOrder;
+
 use config::Config;
-use lexer;
-use lexer::Lexer;
-use lexer::Tag;
+use varint;
+use varint::VarintReaderExt;
 
 #[derive(Debug)]
 pub enum Error {
     InvalidType,
-    InvalidRef(u64),
+    InvalidRef(usize),
     InvalidCopy,
+    UnexpectedEof,
     ArrayTooLarge { count: u64, limit: u64 },
     HashTooLarge { count: u64, limit: u64 },
-    LexerError(lexer::Error),
+    VarintError(varint::Error),
 }
 
 impl Error {
     pub fn is_eof(&self) -> bool {
         match *self {
-            Error::LexerError(ref e) => e.is_eof(),
+            Error::UnexpectedEof => true,
             _ => false,
         }
     }
@@ -32,9 +36,9 @@ impl Error {
     }
 }
 
-impl From<lexer::Error> for Error {
-    fn from(e: lexer::Error) -> Self {
-        Error::LexerError(e)
+impl From<varint::Error> for Error {
+    fn from(e: varint::Error) -> Self {
+        Error::VarintError(e)
     }
 }
 
@@ -91,17 +95,19 @@ pub trait Builder<'buf> {
 
 pub struct Parser<'a, 'buf, B: Builder<'buf>> {
     config: &'a Config,
-    lexer: Lexer<'a, 'buf>,
-    track: HashMap<u64, B::Value>,
+    input: &'buf [u8],
+    pos: usize,
+    track: HashMap<usize, B::Value>,
     builder: B,
-    copy_pos: u64,
+    copy_pos: usize,
 }
 
 impl<'a, 'buf, B: Builder<'buf>> Parser<'a, 'buf, B> {
     pub fn new(builder: B, config: &'a Config, input: &'buf [u8]) -> Parser<'a, 'buf, B> {
         Parser {
-            lexer: Lexer::new(config, input),
             config: config,
+            input: input,
+            pos: 0,
             track: HashMap::new(),
             builder: builder,
             copy_pos: 0,
@@ -121,71 +127,171 @@ impl<'a, 'buf, B: Builder<'buf>> Parser<'a, 'buf, B> {
     }
 
     fn parse_inner(&mut self, force_track: bool) -> Result<B::Value> {
-        let token = self.lexer.next()?;
+        use sereal_common::constants::*;
+
+        let tag = self.read_tag()?;
+
+        let track = tag & TRACK_BIT != 0;
+        let tag = tag & TYPE_MASK;
 
         let mut value = self.builder.new();
 
-        if token.track || force_track {
-            self.track.insert(token.pos, value.clone());
+        if track || force_track {
+            self.track.insert(self.pos, value.clone());
         }
 
-        match token.tag {
-            Tag::Undef => value.set_undef(),
-            Tag::CanonicalUndef => value.set_undef(),
+        match tag {
+            UNDEF | CANONICAL_UNDEF => value.set_undef(),
 
-            Tag::Pos(v) => value.set_u64(v as u64),
-            Tag::Neg(v) => value.set_i64(v as i64),
-            Tag::Varint(v) => value.set_u64(v),
-            Tag::Zigzag(v) => value.set_i64(v),
-            Tag::Float(v) => value.set_f32(v),
-            Tag::Double(v) => value.set_f64(v),
+            POS_0...POS_15 => value.set_u64(tag as u64),
+            NEG_16...NEG_1 => value.set_i64((tag | 0xf0) as i64),
 
-            Tag::True => value.set_true(),
-            Tag::False => value.set_false(),
+            VARINT => value.set_u64(self.read_varint()?),
+            ZIGZAG => value.set_i64(self.read_zigzag()?),
+            FLOAT => value.set_f32(self.read_f32()?),
+            DOUBLE => value.set_f64(self.read_f64()?),
 
-            Tag::Refn => value.set_ref(self.parse()?),
-            Tag::Refp(p) => value.set_ref(self.get(p)?),
-            Tag::Alias(p) => value.set_alias(self.get(p)?),
-            Tag::Copy(pos) => {
-                self.enter_copy(pos)?;
+            TRUE => value.set_true(),
+            FALSE => value.set_false(),
+
+            REFN => value.set_ref(self.parse()?),
+            REFP => {
+                let p = self.read_varint()? as usize;
+                value.set_ref(self.get(p)?);
+            },
+            ALIAS => {
+                let p = self.read_varint()? as usize;
+                value.set_alias(self.get(p)?)
+            },
+            COPY => {
+                if self.copy_pos != 0 {
+                    return Err(Error::InvalidCopy);
+                }
+
+                let pos = self.read_varint()? as usize;
+                self.copy_pos = self.pos;
+                self.pos = pos - 1;
+
                 let val = self.parse()?;
-                self.leave_copy()?;
+
+                self.pos = self.copy_pos;
+                self.copy_pos = 0;
 
                 value.set_alias(val);
             },
-            Tag::Weaken => value.set_weak_ref(self.parse()?),
-
-            Tag::Array(c) => value.set_array(self.parse_array(c)?),
-            Tag::ArrayRef(c) => {
-                let array = self.parse_array(c as u64)?;
+            WEAKEN => value.set_weak_ref(self.parse()?),
+            ARRAY => {
+                let len = self.read_varint()?;
+                value.set_array(self.parse_array(len)?);
+            },
+            ARRAYREF_0...ARRAYREF_15 => {
+                let len = tag - ARRAYREF_0;
+                let array = self.parse_array(len as u64)?;
                 let mut inner = self.builder.new();
                 inner.set_array(array);
                 value.set_ref(inner);
             },
-            Tag::Hash(c) => value.set_hash(self.parse_hash(c)?),
-            Tag::HashRef(c) => {
-                let hash = self.parse_hash(c as u64)?;
+            HASH => {
+                let len = self.read_varint()?;
+                value.set_hash(self.parse_hash(len)?);
+            },
+            HASHREF_0...HASHREF_15 => {
+                let len = tag - HASHREF_0;
+                let hash = self.parse_hash(len as u64)?;
                 let mut inner = self.builder.new();
                 inner.set_hash(hash);
                 value.set_ref(inner);
             },
 
-            Tag::Bin(v) => value.set_binary(&v),
-            Tag::Str(v) => value.set_string(&v),
+            BINARY => {
+                let len = self.read_varint()?;
+                let beg = self.pos;
+                self.pos += len as usize;
+                value.set_binary(&self.input[beg..self.pos]);
+            },
 
-            Tag::Object => value.set_object(self.parse_str(true)?, self.parse()?)?,
-            Tag::ObjectV(o) => value.set_object(self.get(o)?, self.parse()?)?,
-            Tag::ObjectFreeze => value.set_object_freeze(self.parse_str(true)?, self.parse()?)?,
-            Tag::ObjectVFreeze(o) => value.set_object_freeze(self.get(o)?, self.parse()?)?,
+            STR_UTF8 => {
+                let len = self.read_varint()?;
+                let beg = self.pos;
+                self.pos += len as usize;
+                value.set_string(&self.input[beg..self.pos]);
+            },
 
-            Tag::Regexp => value.set_regexp(self.parse()?, self.parse()?)?,
+            SHORT_BINARY_0...SHORT_BINARY_31 => {
+                let len = tag - SHORT_BINARY_0;
+                let beg = self.pos;
+                self.pos += len as usize;
+                value.set_binary(&self.input[beg..self.pos]);
+            },
+
+            OBJECT => value.set_object(self.parse_str(true)?, self.parse()?)?,
+            OBJECTV => {
+                let pos = self.read_varint()? as usize;
+                value.set_object(self.get(pos)?, self.parse()?)?;
+            },
+            OBJECT_FREEZE => value.set_object_freeze(self.parse_str(true)?, self.parse()?)?,
+            OBJECTV_FREEZE => {
+                let pos = self.read_varint()? as usize;
+                value.set_object_freeze(self.get(pos)?, self.parse()?)?;
+            },
+
+            REGEXP => value.set_regexp(self.parse()?, self.parse()?)?,
+
+            _ => unimplemented!(),
         };
 
         Ok(value)
     }
 
-    fn get(&self, p: u64) -> Result<B::Value> {
+    fn get(&self, p: usize) -> Result<B::Value> {
         self.track.get(&p).cloned().ok_or(Error::InvalidRef(p))
+    }
+
+    fn read_tag(&mut self) -> Result<u8> {
+        loop {
+            use sereal_common::constants::{ TYPE_MASK, PAD };
+            if self.pos >= self.input.len() {
+                return Err(Error::UnexpectedEof);
+            }
+
+            let tag = self.input[self.pos];
+            self.pos += 1;
+            if tag & TYPE_MASK != PAD {
+                return Ok(tag);
+            }
+        }
+    }
+
+    fn read_f32(&mut self) -> Result<f32> {
+        let buf = &self.input[self.pos..];
+        if buf.len() < 4 {
+            return Err(Error::UnexpectedEof);
+        }
+        self.pos += 4;
+        Ok(LittleEndian::read_f32(buf))
+    }
+
+    fn read_f64(&mut self) -> Result<f64> {
+        let buf = &self.input[self.pos..];
+        if buf.len() < 8 {
+            return Err(Error::UnexpectedEof);
+        }
+        self.pos += 8;
+        Ok(LittleEndian::read_f64(buf))
+    }
+
+    fn read_varint(&mut self) -> Result<u64> {
+        let mut cursor = io::Cursor::new(&self.input[self.pos..]);
+        let val = cursor.read_varint()?;
+        self.pos += cursor.position() as usize;
+        Ok(val)
+    }
+
+    fn read_zigzag(&mut self) -> Result<i64> {
+        let mut cursor = io::Cursor::new(&self.input[self.pos..]);
+        let val = cursor.read_zigzag()?;
+        self.pos += cursor.position() as usize;
+        Ok(val)
     }
 
     fn parse_array(&mut self, count: u64) -> Result<<B::Value as Value<'buf>>::Array> {
@@ -213,22 +319,6 @@ impl<'a, 'buf, B: Builder<'buf>> Parser<'a, 'buf, B> {
             m.insert(k, v)?;
         }
         Ok(m.finalize())
-    }
-
-    fn enter_copy(&mut self, pos: u64) -> Result<()> {
-        if self.copy_pos != 0 {
-            return Err(Error::InvalidCopy);
-        }
-
-        self.copy_pos = self.lexer.tell()?;
-        self.lexer.seek(pos)?;
-        Ok(())
-    }
-
-    fn leave_copy(&mut self) -> Result<()> {
-        self.lexer.seek(self.copy_pos)?;
-        self.copy_pos = 0;
-        Ok(())
     }
 }
 
